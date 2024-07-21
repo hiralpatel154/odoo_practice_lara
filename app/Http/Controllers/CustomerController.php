d800b4ebb5e88b22f1ee68e84bd1a50d5ec1c3cf
hiralpatel154 <hiraldpatel15497@gmail.com>
2024-07-21 05:33:06 +0530

    find vs where, Eloquent ORM(find($cid)) method vs Eloquent Query Builder method(where(cid,$cid)), log the queries of eloqoent methods, easy solution to find all data of Customer based on customer id

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -3,9 +3,8 @@
 namespace App\Http\Controllers;
 use App\Models\Customer;
 use App\Models\Markup;
-
-
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CustomerController extends Controller
 {
@@ -14,10 +13,22 @@ class CustomerController extends Controller
         return view('index',['customers'=>$cust]);
     }
     public function markup(Request $request){
-        $cid = $request->query('id');
-        $markup = Markup::all()->where('cid',$cid);
+        DB::enableQueryLog();
 
+        $cid = $request->query('id');
         $customer = Customer::find($cid);
+        $markup = Markup::where('cid',$cid)->get();
+
+        $queries = DB::getQueryLog(); // Get query log
+        // Log or dd the queries to inspect
+        // dd($queries);
+    
         return view('markup',['markups'=>$markup,'customer' => $customer]);
+
+        /** find() vs where() in laravel
+            Single Record: Use find($id) when you know you need only one record by its primary key.
+            
+            Multiple Records: Use where('field', $value)->get() when you need multiple records that match a certain condition.
+         * **/
     }
 }
